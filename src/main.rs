1ab8a0725a8e79e9c44c5e443402d2a1f176cd5b
Paulo Diovani Gonçalves <1295197+paulodiovani@users.noreply.github.com>
2024-06-28 13:37:29 -0300

    Use env vars ns split logic into functions

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
-use std::{error::Error, fs::File, io::Read};
+use std::{error::{self}, fs::File, io::{self, Read}};
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let histfile = "/Users/diovani/.zsh_history";
+#[derive(Debug)]
+struct UnkownShell;
 
-    let mut file = File::open(histfile)?;
-    let mut buf = vec![];
-    file.read_to_end (&mut buf)?;
-    let contents = String::from_utf8_lossy (&buf);
-
-    let mut lines: Vec<&str> = contents.lines().collect();
-    lines.sort();
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let histfile = history_file().expect("This program supports: `bash`, `zsh`.");
+    let lines = read_lines_sorted(histfile)?;
 
     println!("{}", lines.join("\n"));
 
     Ok(())
+}
+
+fn history_file() -> Result<String, UnkownShell> {
+    let home = env!("HOME");
+    let shell = env!("SHELL");
+
+    match shell {
+        "/bin/bash" => Ok(format!("{}/.bash_history", home)),
+        "/bin/zsh" => Ok(format!("{}/.zsh_history", home)),
+        _ => Err(UnkownShell),
+    }
+}
+
+fn read_lines_sorted(file: String) -> Result<Vec<String>, io::Error> {
+    let mut file = File::open(file).expect("History file not found");
+    let mut buf = vec![];
+    file.read_to_end(&mut buf)?;
+    let contents = String::from_utf8_lossy(&buf);
+
+    let mut lines: Vec<String> = contents.lines().map(String::from).collect();
+    lines.sort();
+
+    Ok(lines)
 }
