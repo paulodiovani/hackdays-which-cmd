d8ab633223e564aa65ff47b834b916a0fb928a4b
Paulo Diovani Gonçalves <1295197+paulodiovani@users.noreply.github.com>
2024-06-27 21:52:32 -0300

    Read history file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, fs::File, io::Read};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let histfile = "/Users/diovani/.zsh_history";
+
+    let mut file = File::open(histfile)?;
+    let mut buf = vec![];
+    file.read_to_end (&mut buf)?;
+    let contents = String::from_utf8_lossy (&buf);
+
+    let mut lines: Vec<&str> = contents.lines().collect();
+    lines.sort();
+
+    println!("{}", lines.join("\n"));
+
+    Ok(())
 }
